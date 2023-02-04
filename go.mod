5afed2343e55e235fae5f051dffc63cfb28f430d
Nathan Schoedl <nathanschoedl@ufl.edu>
2023-02-04 13:25:37 -0500

    Added DB and HTTP server setup

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.19
 
 require github.com/gorilla/mux v1.8.0
 
-require github.com/rs/cors v1.8.3 // indirect
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/rs/cors v1.8.3 // indirect
+)
---
+++ src/server/api/server.go
@@ -0,0 +1,83 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+)
+
+type Response struct {
+	ID uuid.UUID `json:"ID"`
+	R1 int       `json:"R1"`
+	R2 int       `json:"R2"`
+	R3 int       `json:"R3"`
+	R4 int       `json:"R4"`
+}
+
+type Server struct {
+	*mux.Router
+
+	Responses_DB []Response
+}
+
+func NewServer() *Server {
+	s := &Server{
+		Router:       mux.NewRouter(),
+		Responses_DB: []Response{},
+	}
+	return s
+}
+
+func (s *Server) routes() {
+	s.HandleFunc("/response", s.ListResponses()).Methods("GET")
+	s.HandleFunc("/respone", s.CreateResponse()).Methods("POST")
+	s.HandleFunc("/response/(id}", s.removeResponse()).Methods("DELETE")
+}
+
+func (s *Server) CreateResponse() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var i Response
+		if err := json.NewDecoder(r.Body).Decode(&i); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		i.ID = uuid.New()
+		s.Responses_DB = append(s.Responses_DB, i)
+
+		w.Header().Set("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(i); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func (s *Server) ListResponses() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(s.Responses_DB); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func (s *Server) removeResponse() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		idStr, _ := mux.Vars(r)["id"]
+		id, err := uuid.Parse(idStr)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+
+		for i, item := range s.Responses_DB {
+			if item.ID == id {
+				s.Responses_DB = append(s.Responses_DB[:i], s.Responses_DB[i+1:]...)
+				break
+			}
+		}
+	}
+}
