9a05e26ea63c6b09cd66f0ababc9e26728395972
Ursa Pillay <ursa.pillay@gmail.com>
2023-02-07 15:03:49 -0500

    Add submit button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './question/question.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatButtonModule } from '@angular/material/button'
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -19,7 +22,10 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
+    ReactiveFormsModule,
+    FormsModule,
     MatButtonToggleModule,
+    MatButtonModule,
     MatToolbarModule
   ],
   providers: [],
--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'question',
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.css']
+  styleUrls: ['./question.component.css'],
 })
-export class QuestionComponent {
+export class QuestionComponent{
     title = "Questions: ";
+
+    onSubmit(data: string, data2: string) {
+      console.log(data, data2);
+    }
 }
