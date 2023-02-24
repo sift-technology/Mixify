d14cffd993481e92609781f0ed2b359891378b7c
aarti2626 <aartikalamangalam@gmail.com>
2023-02-24 15:02:05 -0500

    attempt http client request

--- src/server/api/server.go
+++ src/server/api/server.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/google/uuid"
@@ -39,8 +40,10 @@ func (s *Server) routes() {
 }
 
 func (s *Server) CreateResponse() http.HandlerFunc {
+
 	return func(w http.ResponseWriter, r *http.Request) {
 		var i Response
+		fmt.Println("reached")
 		if err := json.NewDecoder(r.Body).Decode(&i); err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
@@ -48,7 +51,7 @@ func (s *Server) CreateResponse() http.HandlerFunc {
 
 		i.ID = uuid.New()
 		s.Responses_DB = append(s.Responses_DB, i)
-
+		fmt.Println(i.R1)
 		w.Header().Set("Content-Type", "application/json")
 		if err := json.NewEncoder(w).Encode(i); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
