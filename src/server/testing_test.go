a25897f5c06909ceb7e3f930361fdb44083a4ac8
aarti2626 <aartikalamangalam@gmail.com>
2023-02-26 11:28:40 -0500

    create JSON response test

--- src/server/testing_test.go
+++ src/server/testing_test.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"testing"
+
+	"github.com/aarti2626/Mixify/src/server/api"
 )
 
-func TestAbs(t *testing.T) {
-	got := -1
-	if got != 1 {
-		t.Errorf("-1 = %d; want 1", got)
-		fmt.Println("Error")
+func TestValidateJSONResponse(t *testing.T) {
+	var i api.Response
+	var user = make(map[string]json.Number)
+	user["R1"] = `json:"0"`
+	user["R2"] = `json:"1"`
+	user["R3"] = `json:"2"`
+	user["R4"] = `json:"300"`
+	i.R1 = user["R1"]
+	i.R2 = user["R2"]
+	i.R3 = user["R3"]
+	i.R4 = user["R4"]
+	if i.R2 != user["R2"] {
+		t.Errorf("Incompatible cast from json.Number to int")
+	} else {
+		fmt.Println("Test succeeded")
 	}
+
 }
