d2725b28e8ebed24699c313535b389bde97942ba
Ursa Pillay <ursa.pillay@gmail.com>
2023-03-27 16:14:31 -0400

    update questions

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -12,7 +12,7 @@ import { Router } from '@angular/router';
 export class QuestionComponent{
     title = "Questions: ";
     disabled = false;
-    max = 100;
+    max = 10;
     min = 0;
     showTicks = false;
     step = 1;
@@ -23,13 +23,15 @@ export class QuestionComponent{
 
     public RESULTS: number[] = []
 
-    onSubmit(data1: number, data2: number, data3: number, data4: number) {
+    onSubmit(data1: number, data2: number, data3: number, data4: number, data5: number, data6: number) {
       this.router.navigateByUrl('/results');
-      console.log(data1, data2, data3, data4);
+      console.log(data1, data2, data3, data4, data5, data6);
       this.RESULTS[0] = data1;
       this.RESULTS[1] = data2;
       this.RESULTS[2] = data3;
       this.RESULTS[3] = data4;
+      this.RESULTS[4] = data5;
+      this.RESULTS[5] = data6;
       this.myservice.getList(this.RESULTS);
       console.log(this.RESULTS);
     }
