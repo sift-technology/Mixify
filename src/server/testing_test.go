9fd3e22d0bb8cd7b75855e9076d96afdef57c556
Nathan Schoedl <nathanschoedl@ufl.edu>
2023-03-28 14:28:37 -0400

    Create new test for spotify api to recommend songs

--- src/server/testing_test.go
+++ src/server/testing_test.go
@@ -41,23 +41,34 @@ func TestCreateUniqueUUID(t *testing.T) {
 	fmt.Println("Test passed: unique UUID created")
 }
 
+// test all 163,840 possible responses and sees if spotify will recommend tracks for all possible answer combinations
 func TestWeightFunct(t *testing.T) {
 
 	var user api.Response
+	var userRec api.Rec
+	s := api.NewServer()
 
-	for i3 := 1; i3 <= 100; i3++ {
+	for i3 := 0; i3 <= 10; i3++ {
 		for i1 := 1; i1 <= 4; i1++ {
 			for i2 := 1; i2 <= 4; i2++ {
 				for i4 := 1; i4 <= 4; i4++ {
-					user.R1 = json.Number(strconv.FormatInt(int64(i1), 10))
-					user.R2 = json.Number(strconv.FormatInt(int64(i2), 10))
-					user.R3 = json.Number(strconv.FormatInt(int64(i3), 10))
-					user.R4 = json.Number(strconv.FormatInt(int64(i4), 10))
-					api.Weights(&user)
+					for i5 := 1; i4 <= 4; i4++ {
+						for i6 := 1; i4 <= 4; i4++ {
 
-					if user.M.Danceability <= 0 || user.M.Danceability >= 1 || user.M.Energy <= 0 || user.M.Energy >= 1 ||
-						user.M.Popularity <= 0 || user.M.Popularity >= 100 || user.M.Acousticness <= 0 || user.M.Acousticness >= 1 {
-						t.Errorf("Metric out of range")
+							user.R1 = json.Number(strconv.FormatInt(int64(i1), 10))
+							user.R2 = json.Number(strconv.FormatInt(int64(i2), 10))
+							user.R3 = json.Number(strconv.FormatInt(int64(i3), 10))
+							user.R4 = json.Number(strconv.FormatInt(int64(i4), 10))
+							user.R5 = json.Number(strconv.FormatInt(int64(i5), 10))
+							user.R6 = json.Number(strconv.FormatInt(int64(i6), 10))
+							api.Weights(&user)
+							userRec.Recs = api.Recommend(s.Client, &user)
+
+							if len(userRec.Recs) == 0 {
+								t.Errorf("No recommendations")
+							}
+						}
+
 					}
 				}
 			}
